e836d2d0ecc5387a39dcc6139a0f02dd7713d4e4
Josh Gude <gude.joshua@gmail.com>
2018-09-18 00:50:46 -0400

    project complete

---
+++ src/App/App.js
@@ -0,0 +1,144 @@
+import React, { Component } from 'react';
+import './App.css';
+import ListElement from '../ListElement/ListElement';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      wipItems: [],
+      completeItems: []
+    }
+  }
+
+  /*---- WIP LIST METHODS ----*/
+
+  /* Handles submission of text input and adds a ListElement component to the WIP list
+     in this.state  */
+  handleSubmit(e) {
+    e.preventDefault();
+    const inputContent = e.target.firstChild.value;
+    const newItemsArray = this.state.wipItems.slice(0, this.state.wipItems.length);
+    newItemsArray.push({content: inputContent});
+    this.setState({wipItems: newItemsArray});
+    e.target.firstChild.value = '';
+  }
+
+  /* Handles user clicking the delete button within the WIP list. Removes the task from all lists. */
+  handleWipDelete(e, index) {
+    const newArr = this.state.wipItems.filter((li, i) => i !== index);
+    this.setState({wipItems: newArr});
+  }
+
+  /* Handles user clicking the complete task button within the WIP list. Transfers
+     ListElement component to the Complete list. */
+  handleCompleteTask(e, content) {
+    const newWipArr = this.state.wipItems.filter(li => li.content !== content);
+    const newCompleteArr = this.state.completeItems.concat([{content: content}]);
+
+    this.setState({wipItems: newWipArr, completeItems: newCompleteArr});
+  }
+
+  /*---- COMPLETED LIST METHODS ----*/
+
+  /* Handles user clicking the uncomplete task button within the Completed list.
+     Transfers ListElement component to the WIP list. */
+  handleUncompleteTask(e, content) {
+    const newCompleteArr = this.state.completeItems.filter(li => li.content !== content);
+    const newWipArr = this.state.wipItems.concat([{content: content}]);
+
+    this.setState({wipItems: newWipArr, completeItems: newCompleteArr});
+  }
+
+  /* Handles user clicking on the delete button within the Completed list.
+     Removes the task from all lists. */
+  handleCompleteDelete(e, index) {
+    const newArr = this.state.completeItems.filter((li, i) => i !== index);
+    this.setState({completeItems: newArr});
+  }
+
+  /*---- DRAG AND DROP METHODS ----*/
+
+  /* Captures content of the target of the Drag action so that it can be
+     used in the drop methods */
+  handleDragStart(e, content) {
+    e.dataTransfer.setData("content", content);
+  }
+
+  /* Nullifies default behvior of dragOver event */
+  handleDragOver(e) {
+    e.preventDefault();
+  }
+
+  /* Filters out duplicate drops then transfers ListElement component from
+     WIP list to Complete list */
+  handleDropToComplete(e) {
+    for (let i = 0; i < this.state.completeItems.length; i++) {
+      if (e.dataTransfer.getData("content") === this.state.completeItems[i].content) {
+        return -1;
+      }
+    }
+    const content = e.dataTransfer.getData("content");
+    const newWipArr = this.state.wipItems.filter(li => li.content !== content);
+    const newCompleteArr = this.state.completeItems.concat([{content: content}])
+    this.setState({wipItems: newWipArr, completeItems: newCompleteArr});
+  }
+
+  /* Filters out duplicate drops then transfers ListElement component from
+     Complete list to WIP list */
+  handleDropToWip(e) {
+    for (let i = 0; i < this.state.wipItems.length; i++) {
+      if (e.dataTransfer.getData("content") === this.state.wipItems[i].content) {
+        return -1;
+      }
+    }
+    const content = e.dataTransfer.getData("content");
+    const newCompleteArr = this.state.completeItems.filter(li => li.content !== content);
+    const newWipArr = this.state.wipItems.concat([{content: content}]);
+    this.setState({wipItems: newWipArr, completeItems: newCompleteArr})
+  }
+
+  
+
+  render() {
+    return (
+      <div className="App">
+        <h1>To Do</h1>
+        <div className="lists">
+          <ul className="wip" onDragOver={e => this.handleDragOver(e)} onDrop={e => this.handleDropToWip(e)}>
+            <h2>Work In Progress</h2>
+            {this.state.wipItems.map((li, index) => 
+              <ListElement 
+              key={li.content} 
+              content={li.content} 
+              finished={false}
+              handleDelete={(e) => this.handleWipDelete(e, index)} 
+              handleComplete={(e) => this.handleCompleteTask(e, li.content)}
+              onDragStart={e => this.handleDragStart(e, li.content)} />
+            )}
+            <li className="edit-task">
+              <form onSubmit={this.handleSubmit.bind(this)}>
+                <input type="text" placeholder="add new task.." />
+              </form>
+            </li>
+          </ul>
+          <ul className="completed" onDragOver={e => this.handleDragOver(e)} onDrop={(e) => this.handleDropToComplete(e)}>
+              <h2>Completed Tasks</h2>
+              {this.state.completeItems.map((li, index) => 
+              <ListElement 
+              key={li.content} 
+              content={li.content} 
+              finished={true} 
+              handleDelete={(e) => this.handleCompleteDelete(e, index)} 
+              handleUncomplete={(e) => this.handleUncompleteTask(e, li.content)}
+              onDragStart={e => this.handleDragStart(e, li.content)}
+              />
+              )}
+          </ul>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/ListElement/ListElement.js
@@ -0,0 +1,68 @@
+import React, { Component } from 'react';
+
+class ListElement extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            edit: false,
+            content: this.props.content,
+            finished: this.props.finished
+        }
+    }
+
+    /* Handles user clicking the edit button. Causes re-render to display
+       a text input in place of the final task contents. */
+    handleEdit() {
+        if (this.state.edit === false) {
+            this.setState({edit: true});
+        } else {
+            this.setState({edit: true});
+        }
+    }
+
+    /* Handles submission of text input on a ListElement with this.state.edit set
+       to true. */
+    handleSubmit(e) {
+        e.preventDefault();
+        const inputContent = e.target.firstChild.value;
+        this.setState({edit: false, content: inputContent});
+    }
+
+    render() {
+        if (this.state.edit === false) {
+            if (this.state.finished === false) {
+                return (
+                    <li className="saved-task" draggable onDragStart={this.props.onDragStart} >
+                        {this.state.content}
+                        <div className="buttons">
+                            <span><button className="buttons" id="finish" onClick={this.props.handleComplete}><ion-icon name="checkmark"></ion-icon></button></span>
+                            <span><button className="buttons" id="edit" onClick={this.handleEdit.bind(this)}><ion-icon name="build"></ion-icon></button></span>
+                            <span><button className="buttons" id="delete" onClick={this.props.handleDelete}><ion-icon name="trash"></ion-icon></button></span>
+                        </div>
+                    </li>
+                )
+            } else {
+                return (
+                    <li className="saved-task finished" draggable onDragStart={this.props.onDragStart}>
+                        {this.state.content}
+                        <div className="buttons">
+                            <span><button className="buttons" id="finish" onClick={this.props.handleUncomplete}><ion-icon name="undo"></ion-icon></button></span>
+                            <span><button className="buttons" id="edit" onClick={this.handleEdit.bind(this)}><ion-icon name="build"></ion-icon></button></span>
+                            <span><button className="buttons" id="delete" onClick={this.props.handleDelete}><ion-icon name="trash"></ion-icon></button></span>
+                        </div>
+                    </li>
+                );
+            }
+        } else {
+            return (
+                <li className="edit-task">
+                    <form onSubmit={this.handleSubmit.bind(this)}>
+                        <input type="text" placeholder={this.state.content} autoFocus/>
+                    </form>
+                </li>
+            );
+        }
+    }
+}
+
+export default ListElement;
