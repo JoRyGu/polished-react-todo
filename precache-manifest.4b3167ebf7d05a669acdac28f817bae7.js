24a08fd3f7fa3f64133390262fbdc35babb61557
Josh Gude <gude.joshua@gmail.com>
2019-01-15 03:05:55 -0500

    Updates

---
+++ precache-manifest.4b3167ebf7d05a669acdac28f817bae7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f6c8df0c349fb7c4a4c1",
+    "url": "/polished-react-todo/static/js/runtime~main.f6c8df0c.js"
+  },
+  {
+    "revision": "77fb8a4a444cbd3276b7",
+    "url": "/polished-react-todo/static/js/main.77fb8a4a.chunk.js"
+  },
+  {
+    "revision": "97f165e70af863d8fb37",
+    "url": "/polished-react-todo/static/js/1.97f165e7.chunk.js"
+  },
+  {
+    "revision": "77fb8a4a444cbd3276b7",
+    "url": "/polished-react-todo/static/css/main.73a6cd9b.chunk.css"
+  },
+  {
+    "revision": "e1377ac7ce034647b47307ec03627a36",
+    "url": "/polished-react-todo/index.html"
+  }
+];
